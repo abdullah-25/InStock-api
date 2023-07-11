d49cb4e46ac8a629d43d0cd6d909e0de208b5707
abdullah-25 <imv.abdullah@mail.utoronto.ca>
2023-07-11 16:29:32 -0400

    backend repo created

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const videos = require("./routes/videos");
+const app = express();
+
+const PORT = process.env.PORT || 5050;
+
+// middleware
+app.use(express.json());
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(express.static("public"));
+
+// register routes
+app.use("/videos", videos);
+
+// go!!
+app.listen(PORT, () => {
+  console.log("server started on port", PORT);
+});
