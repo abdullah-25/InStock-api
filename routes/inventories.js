27f572e87c399f67fa869de62470db8b7b1e3578
Zachary Ringwood <zringwood@gmail.com>
2023-07-13 14:41:00 -0400

    Changed route to use knex query builder

--- routes/inventories.js
+++ routes/inventories.js
@@ -5,7 +5,9 @@ const fs = require("node:fs");
 const knex = require('knex')(require('../knexfile'));
 
 function getInventories(req, res){
-    knex.raw("select warehouse_name, inventories.* from warehouses left join inventories on warehouses.id = inventories.warehouse_id;").then((response) => {
+    knex('warehouses')
+  .leftJoin('inventories', 'warehouses.id', '=', 'inventories.warehouse_id')
+  .select('warehouse_name', 'inventories.*').then((response) => {
         return res.status(200).send(response);
     }).catch((response) => {
         console.log(response);
